d811abaee70a010ab0588df8f0035da93d2f2864
secureWKkim <1rladnjsrud1@gmail.com>
2024-07-23 02:04:32 +0900

    refactor(BE): APP 명칭 jaringobi에서 search로 변경(#1)

--- jaringobi_be/jaringobi/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-
-from .views import *
-
-urlpatterns = [
-    path('', home),  # https://wikidocs.net/70649
-    path('search/', search),
-    # path('api/', include('rest_framework.urls')),
-]
--- jaringobi_be/search/apps.py
+++ jaringobi_be/search/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class JaringobiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'jaringobi'
+    name = 'search'
