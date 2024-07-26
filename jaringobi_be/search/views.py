b63925506eafe882594682980ee2c72719a06664
secureWKkim <1rladnjsrud1@gmail.com>
2024-07-26 09:58:47 +0900

    feat(BE): 검색 결과 응답 View  구현 (#1)

--- jaringobi_be/search/views.py
+++ jaringobi_be/search/views.py
@@ -1,11 +1,12 @@
 from django.http import Http404
 
 from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
 from rest_framework import generics
 from rest_framework.decorators import api_view
 
-from .models import CheapRecipe, CheapestProduct
-from .serializers import SearchResponseSerializer
+from .models import CheapRecipe
+from .serializers import CheapRecipeSerializer
 
 
 # https://wikidocs.net/70649
@@ -15,26 +16,17 @@ def home(request):
 
 
 # TODO: 나중에 get+post=ListCreateAPIView(메뉴 1개 찾고 없으면 post)로 리팩토링
-# 오버라이드 할 수 있는 다른 메서드들도 알아보기
+# 오버라이드 할 수 있는 다른 메서드도 많다.
 class SearchResponseView(generics.RetrieveAPIView):  # get(read-one)
-    serializer_class = SearchResponseSerializer
+    queryset = CheapRecipe.objects.all()
+    serializer_class = CheapRecipeSerializer
+    lookup_field = 'menu'  # Use the menu field for lookup instead of pk
 
-    # def get(self, request, pk, format=None):
     def get_object(self):
-        try:
-            menu = self.request.query_params.get('search', None)  # URL query parameter 로 넘어온 메뉴명 get
-            # 해당 메뉴명과 정확히 일치하는 CheapRecipe 테이블 row 1개 반환
-            recipe = CheapRecipe.objects.get(menu=menu)  # TODO: (도균님) 정확하게 일치하는 결과만을 찾지 않도록, 유연한 검색 로직 개발
-        except CheapRecipe.DoesNotExist:
-            return Response({'error': 'Recipe not found'}, status=404)
-
-        serializer = SearchResponseSerializer(recipe)
-        return Response(serializer.data)
-
-    # def get_queryset(self):
-    #     queryset = CheapRecipe.objects.all()
-    #     menu = self.request.query_params.get('search', None)
-    #     if menu:
-    #         # 여러개면 filter. 그리고 검색 기능 정교함에 따라 이거 갖고 안될 수 있음. menu__icontians는 lookup 찾아보면 나오고, case-insensitive match 말하는거임.
-    #         queryset = queryset.filter(title__icontains=menu)
-    #     return queryset
+        menu = self.kwargs.get('menu') # TODO: (도균님) 정확하게 일치하는 결과만을 찾지 않도록, 유연한 검색 로직 개발
+        if menu is not None:
+            try:
+                return self.queryset.get(menu=menu)
+            except CheapRecipe.DoesNotExist:
+                raise NotFound("Menu not found")
+        raise NotFound("Invalid parameter")
