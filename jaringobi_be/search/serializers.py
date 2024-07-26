4f78ebd6fb0524f8b90ac598d5498cd5bc0fbe00
secureWKkim <1rladnjsrud1@gmail.com>
2024-07-26 09:56:48 +0900

    feat(BE): 직렬화 구현. Nested 구조 사용 (#1)

--- jaringobi_be/search/serializers.py
+++ jaringobi_be/search/serializers.py
@@ -1,65 +1,31 @@
 from rest_framework import serializers
-from .models import Ingredient, Product, Recipe, RecipeIngredient, YoutubeVdo, CheapestProduct, CheapRecipe
-
+from .models import Ingredient, Product, Recipe, YoutubeVdo, CheapRecipe
 
 
 # ModelSerializer 객체의 arguments들이 뭐가 더 있는지 보기.
 class IngredientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ingredient
-        fields = "__all__"
-
-
-class RecipeIngredientSerializer(serializers.ModelSerializer):
-    # This serializer will be used to include additional fields from the RecipeIngredient model.
-    ingredients = IngredientSerializer()
-    
-    class Meta:
-        model = RecipeIngredient
-        fields = "__all__"
+        # fields = "__all__"
+        exclude = ("id", "alternative",)
 
 
 class RecipeSerializer(serializers.ModelSerializer):
-    # SerializerMethodField는 메서드 통해 Custom Fields 정의할 떄 사용
-    ingredients = serializers.SerializerMethodField()
-    ingredients_without_unit = serializers.SerializerMethodField()
+    ingredients = IngredientSerializer(many=True, read_only=True)
 
     class Meta:
         model = Recipe
-        fields = "__all__"
-
-    def get_ingredients(self, obj):
-        ingredients = [ri.ingredient for ri in RecipeIngredient.objects.filter(recipe=obj)]
-        return RecipeIngredientSerializer(ingredients, many=True).data
-
-    def get_ingredients_without_unit(self, obj):
-        ingredients_without_unit = Ingredient.objects.filter(recipeingredient__recipe=obj, unit__isnull=True, vague__isnull=False)
-        return RecipeIngredientSerializer(ingredients_without_unit, many=True).data
+        exclude = ("id", "menu_id",)
 
 
-###################################################
 class CheapRecipeSerializer(serializers.ModelSerializer):
-    # include ingredient details.
-    ingredients = RecipeSerializer(many=True)  # Nested serializer: Handling Nested Relationships
-    ingredients_without_unit = RecipeSerializer(many=True)
+    # include ingredient details. Nested serializer: Handling Nested Relationships
+    # ingredients = IngredientSerializer(many=True)  # read_only=True
+    recipe = RecipeSerializer(read_only=True)
     
     class Meta:
         model = CheapRecipe
         exclude = ("id", "created_at", "updated_at",)
-
-
-class CheapestProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CheapestProduct
-        exclude = ("id", "created_at", "updated_at",)
-
-
-# 궁극 목표
-# 한 번에 2개 이상의 모델 사용 가능하면 ModelSerializer 쓰는데, 아니라면...
-# Combining Serializers for Complex Responses
-class SearchResponseSerializer(serializers.Serializer):
-    cheap_recipes = CheapRecipeSerializer(many=True)
-    cheapest_products = CheapestProductSerializer(many=True)
 
 
 """
