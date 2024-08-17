7abcb0604b074b70623fe964fa5b9f151df53ad7
poriz <areacmzl@gmail.com>
2024-08-17 14:35:41 +0900

    feat(BE): CORS 설정(#17)

--- jaringobi_be/search/urls.py
+++ jaringobi_be/search/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import SearchResponseView, home
 
 urlpatterns = [
-    path('search/', home),
-    path('search/<str:menu>', SearchResponseView.as_view(), name='result'),
+    # path('search/', home),
+    path('search/', SearchResponseView.as_view(), name='result'),
 ]
