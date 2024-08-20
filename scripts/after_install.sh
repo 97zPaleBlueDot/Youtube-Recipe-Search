8d3afa51074dfad30aabea0c932e89b0a692f608
poriz <areacmzl@gmail.com>
2024-08-20 16:09:04 +0900

    cicd: add log python path

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,10 +2,13 @@
 
 # TAR 파일을 배포 디렉토리로 이동
 cd /opt/bitnami/BackEnd
-# active venv
+
+# 가상환경 활성화
 source .venv/bin/activate
 
+# Django 프로젝트 디렉토리로 이동
 cd /opt/bitnami/BackEnd/jaringobi_be
+
 # 로그 폴더 경로 설정
 LOG_DIR="server_onoff_logs"
 LOG_FILE="$LOG_DIR/$(date '+%Y-%m-%d_%H-%M-%S').log"
@@ -17,6 +20,9 @@ mkdir -p $LOG_DIR
 {
     echo "==== Django Server Restart ===="
     echo "Timestamp: $(date '+%Y-%m-%d %H:%M:%S')"
+
+    # 활성화된 Python 경로 확인
+    echo "Using Python interpreter at: $(which python)"
 
     # 서버가 실행 중인 프로세스 확인
     echo "Checking for running Django server..."
