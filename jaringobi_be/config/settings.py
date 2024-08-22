fd459aeba6c91d1c7fcd97f6930b3516d6c9751b
Helicobacto <yangju0411@gmail.com>
2024-08-22 19:23:06 +0900

    feat(BE): 웹 요청 url 로깅 (#9)
    url - http method - timestamp 형태의 로그를 DB에 저장

--- jaringobi_be/config/settings.py
+++ jaringobi_be/config/settings.py
@@ -60,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'search.middleware.RequestLogMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
